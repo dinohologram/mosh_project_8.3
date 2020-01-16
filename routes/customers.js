602a27802ee79e64695e2982155dcef761605dbf
John <johnmsaylor@gmail.com>
2020-01-16 12:52:32 -0500

    validate

--- routes/customers.js
+++ routes/customers.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validateCustomer(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   let customer = new Customer({ 
@@ -24,7 +24,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateCustomer(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   const customer = await Customer.findByIdAndUpdate(req.params.id,
